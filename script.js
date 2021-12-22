31b52c93c74dd85343d8fd8f53ddd66868fe4d84
sleepntsheep <sheep@papangkorn.com>
2021-12-22 17:18:35 +0700

    shouldwork

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let height = window.innerHeight
 let width = window.innerWidth
 let nx = Math.floor(width / cell_size)
 let ny = Math.floor(height / cell_size)
-let presetDOM = document.querySelector('#preset')
 let canvas = document.querySelector('#mycanvas')
 let ctx = canvas.getContext('2d')
 
@@ -18,271 +17,10 @@ class Preset {
 }
 
 let presets = {
-    'square': new Preset(10, 10,
-        [
-            {
-                'value': "2,0"
-            },
-            {
-                'value': "1,0"
-            },
-            {
-                'value': "0,0"
-            },
-            {
-                'value': "0,1"
-            },
-            {
-                'value': "0,2"
-            },
-            {
-                'value': "0,3"
-            },
-            {
-                'value': "0,4"
-            },
-            {
-                'value': "0,5"
-            },
-            {
-                'value': "0,6"
-            },
-            {
-                'value': "0,7"
-            },
-            {
-                'value': "0,8"
-            },
-            {
-                'value': "0,9"
-            },
-            {
-                'value': "1,9"
-            },
-            {
-                'value': "3,9"
-            },
-            {
-                'value': "4,9"
-            },
-            {
-                'value': "2,9"
-            },
-            {
-                'value': "5,9"
-            },
-            {
-                'value': "6,9"
-            },
-            {
-                'value': "7,9"
-            },
-            {
-                'value': "8,9"
-            },
-            {
-                'value': "9,9"
-            },
-            {
-                'value': "9,7"
-            },
-            {
-                'value': "9,8"
-            },
-            {
-                'value': "9,3"
-            },
-            {
-                'value': "9,0"
-            },
-            {
-                'value': "9,1"
-            },
-            {
-                'value': "9,2"
-            },
-            {
-                'value': "9,6"
-            },
-            {
-                'value': "9,4"
-            },
-            {
-                'value': "9,5"
-            },
-            {
-                'value': "8,0"
-            },
-            {
-                'value': "7,0"
-            },
-            {
-                'value': "5,0"
-            },
-            {
-                'value': "4,0"
-            },
-            {
-                'value': "3,0"
-            },
-            {
-                'value': "6,0"
-            },
-        ]),
     
-    '>o<': new Preset(18, 13, [
-    {
-        "value": "1,0"
-    },
-    {
-        "value": "2,0"
-    },
-    {
-        "value": "3,1"
-    },
-    {
-        "value": "4,1"
-    },
-    {
-        "value": "5,2"
-    },
-    {
-        "value": "4,3"
-    },
-    {
-        "value": "3,3"
-    },
-    {
-        "value": "2,4"
-    },
-    {
-        "value": "1,4"
-    },
-    {
-        "value": "1,6"
-    },
-    {
-        "value": "0,7"
-    },
-    {
-        "value": "2,7"
-    },
-    {
-        "value": "3,6"
-    },
-    {
-        "value": "4,8"
-    },
-    {
-        "value": "4,9"
-    },
-    {
-        "value": "4,10"
-    },
-    {
-        "value": "4,11"
-    },
-    {
-        "value": "5,12"
-    },
-    {
-        "value": "6,12"
-    },
-    {
-        "value": "7,11"
-    },
-    {
-        "value": "9,11"
-    },
-    {
-        "value": "10,11"
-    },
-    {
-        "value": "8,11"
-    },
-    {
-        "value": "11,12"
-    },
-    {
-        "value": "12,12"
-    },
-    {
-        "value": "13,11"
-    },
-    {
-        "value": "13,8"
-    },
-    {
-        "value": "13,9"
-    },
-    {
-        "value": "13,10"
-    },
-    {
-        "value": "12,7"
-    },
-    {
-        "value": "11,7"
-    },
-    {
-        "value": "9,8"
-    },
-    {
-        "value": "6,7"
-    },
-    {
-        "value": "5,7"
-    },
-    {
-        "value": "8,8"
-    },
-    {
-        "value": "7,8"
-    },
-    {
-        "value": "10,8"
-    },
-    {
-        "value": "14,6"
-    },
-    {
-        "value": "16,6"
-    },
-    {
-        "value": "15,7"
-    },
-    {
-        "value": "17,7"
-    },
-    {
-        "value": "13,3"
-    },
-    {
-        "value": "14,3"
-    },
-    {
-        "value": "16,4"
-    },
-    {
-        "value": "15,4"
-    },
-    {
-        "value": "12,2"
-    },
-    {
-        "value": "13,1"
-    },
-    {
-        "value": "14,1"
-    },
-    {
-        "value": "15,0"
-    },
-    {
-        "value": "16,0"
-    }
-])
-};
+}
+
+let userPresets = JSON.parse(localStorage.getItem('userPresets')) || {}
 
 let drawBorder = () => {
     ctx.beginPath()
@@ -301,15 +39,15 @@ let drawBorder = () => {
 
 let drawCell = (cord) => {
     ctx.fillStyle = '#FF007F'
-    x = cord.split(',')[0]
-    y = cord.split(',')[1]
+    x = cord.split('-')[0]
+    y = cord.split('-')[1]
     ctx.fillRect(cell_size * x + 1, cell_size * y + 1, cell_size - 1, cell_size - 1)
     ctx.stroke()
 }
 
 let eraseCell = (cord) => {
-    x = cord.split(',')[0]
-    y = cord.split(',')[1]
+    x = cord.split('-')[0]
+    y = cord.split('-')[1]
     ctx.clearRect(cell_size * x + 1, cell_size * y + 1, cell_size - 1, cell_size - 1)
     ctx.stroke()
 }
@@ -329,7 +67,7 @@ let handleMouseClick = (event) => {
     const rect = canvas.getBoundingClientRect()
     x = event.clientX - rect.left
     y = event.clientY - rect.top
-    posXY = `${Math.floor(x / cell_size)},${Math.floor(y / cell_size)}`
+    posXY = `${Math.floor(x / cell_size)}-${Math.floor(y / cell_size)}`
     if (grids.has(posXY)) {
         grids.delete(posXY)
         eraseCell(posXY)
@@ -338,7 +76,6 @@ let handleMouseClick = (event) => {
         grids.add(posXY)
         drawCell(posXY)
     }
-    console.log(grids)
 }
 
 let clearBoard = () => {
@@ -356,28 +93,117 @@ let drawAll = () => {
     }
 }
 
-let loadPreset = (name) => {
+let loadPreset = (preset) => {
     clearBoard()
-    const preset = presets[name]
     const offsetX = Math.floor(nx / 2 - Math.floor(preset.width) / 2)
     const offsetY = Math.floor(ny / 2 - Math.floor(preset.height) / 2)
     for (const cord of preset.grids) {
-        posXY = `${parseInt(cord.value.split(',')[0]) + offsetX},${parseInt(cord.value.split(',')[1]) + offsetY}`
+        console.log(cord)
+        posXY = `${parseInt(cord.split('-')[0]) + offsetX}-${parseInt(cord.split('-')[1]) + offsetY}`
         grids.add(posXY)
         drawCell(posXY)
     }
 }
 
-let addPresetMenu = (preset, name) => {
+let loadPresetName = (name) => {
+    const preset = presets[name]
+    loadPreset(preset)
+}
+
+let loadUserPresetName = (name) => {
+    const preset = userPresets[name]
+    loadPreset(preset)
+}
+
+let addPresetMenu = (preset) => {
+    const presetDOM = document.querySelector('#preset')
+    const element = document.createElement('button');
+    element.textContent = preset.name
+    element.setAttribute('class', 'm-4 bg-green-300 p-4 rounded')
+    element.addEventListener('click', () => { loadPresetName(preset.name) })
+    presetDOM.appendChild(element)
+}
+
+let addUserPresetMenu = (name) => {
+    const presetDOM = document.querySelector('#user-preset')
     const element = document.createElement('button');
     element.textContent = name
     element.setAttribute('class', 'm-4 bg-green-300 p-4 rounded')
-    element.addEventListener('click', () => { loadPreset(name) })
+    element.addEventListener('click', () => { loadUserPresetName(name) })
     presetDOM.appendChild(element)
+}
+
+let exportPreset = () => {
+    const name = prompt('Name this preset')
+    const presetArr = Array.from(grids)
+    let newArr = []
+    const [maxX, minX, maxY, minY] = getWH(presetArr)
+    for (const cord of presetArr) {
+        const x = parseInt(cord.split('-')[0])
+        const y = parseInt(cord.split('-')[1])
+        console.log(x, y)
+        newArr.push(`${x-minX}-${y-minY}`)        
+    }
+    preset = new Preset(maxX-minX, maxY-minY, newArr)
+ 
+    navigator.clipboard.writeText(JSON.stringify({name: name, data: preset})).then(function() {
+        alert('Copied to clipboard')
+      }, function(err) {
+        alert('Could not copy to clipboard', err)
+      });
+}
+
+let importPreset = () => {
+    const presetString = prompt('Input your preset', '')
+    if (presetString == null || presetString == '') {
+        return
+    }
+    const presetObj = JSON.parse(presetString)
+    loadPreset(presetObj.data)
+    savePreset(presetObj.name)
+}
+
+let getWH = (presetArr) => {
+    let minX = 1e9
+    let minY = 1e9
+    let maxX = 0
+    let maxY = 0
+    for (const cord of presetArr) {
+        const x = parseInt(cord.split('-')[0])
+        const y = parseInt(cord.split('-')[1])
+        minX = Math.min(x, minX)
+        minY = Math.min(y, minY)
+        maxX = Math.max(x, maxX)
+        maxY = Math.max(y, maxY)
+    }
+    
+    return [maxX , minX, maxY , minY]
+}
+
+let savePreset = (name = '') => {
+    if (name === ''){
+        name = prompt('Name this preset')
+    }
+    const presetArr = Array.from(grids)
+    let newArr = []
+    const [maxX, minX, maxY, minY] = getWH(presetArr)
+    for (const cord of presetArr) {
+        const x = parseInt(cord.split('-')[0])
+        const y = parseInt(cord.split('-')[1])
+        console.log(x, y)
+        newArr.push(`${x-minX}-${y-minY}`)        
+    }
+    preset = new Preset(maxX-minX, maxY-minY, newArr)
+    userPresets[name] = preset
+    localStorage.setItem('userPresets', JSON.stringify(userPresets))
 }
 
 for (const [key, preset] of Object.entries(presets)) {
     addPresetMenu(preset, key)
+}
+
+for (const [key, preset] of Object.entries(userPresets)) {
+    addUserPresetMenu(key)
 }
 
 resizeCanvas()
@@ -387,8 +213,23 @@ window.addEventListener('resize', resizeCanvas, false)
 let cellSizeInput = document.querySelector('#cell-size-input')
 let applySettingButton = document.querySelector('#apply-button')
 let clearBoardButton = document.querySelector('#clear-button')
+let exportButton = document.querySelector('#export-button')
+let importButton = document.querySelector('#import-button')
+let saveButton = document.querySelector('#save-button')
 
 cellSizeInput.value = cell_size
+
+exportButton.addEventListener('click', (event) => {
+    exportPreset()
+})
+
+importButton.addEventListener('click', (event) => {
+    importPreset()
+})
+
+saveButton.addEventListener('click', (event) => {
+    savePreset()
+})
 
 applySettingButton.addEventListener('click', (event) => {
     cell_size = parseInt(cellSizeInput.value)
